69d4e21284f35becf7fece2d6ce5603074952d27
dianhsu <xudian.cn@gmail.com>
2021-03-09 19:09:24 +0800

    fix

--- model/include/norm.h
+++ model/include/norm.h
@@ -15,21 +15,7 @@ public:
     LayerNorm();
     ~LayerNorm();
     void load_params(LayerNormParam<T, DIM> *p);
-    void forward(T input[DIM], T output[DIM]) {
-        T sum = 0;
-        T sum2 = 0;
-        for(int i = 0; i < DIM; ++i) {
-            sum += input[i];
-            sum2 += input[i] * input[i];
-        }
-        T avg = sum / DIM;
-        T avg2 = sum2 / DIM;
-        T var = avg2 - avg * avg;
-        T sq_var = sqrt(var + 1e-5);
-        for(int i = 0; i < DIM; ++i) {
-            output[i] = (input[i] - avg)/sq_var * (params->weight[i]) + (params->bias[i]);
-        }
-    }
+    void forward(T input[DIM], T output[DIM]);
 private:
     LayerNormParam<T, DIM> *params;
 
