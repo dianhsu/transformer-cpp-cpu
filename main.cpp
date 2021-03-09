d5219917c46e9db6a2820478eea11e6ef3c817ee
dianhsu <xudian.cn@gmail.com>
2021-03-09 19:35:15 +0800

    fix

--- main.cpp
+++ main.cpp
@@ -2,6 +2,8 @@
 
 
 int main() {
+    TransformerParam<float, 512, 2048, 8, 6, 6> param;
     Transformer<float, 512, 20, 2048, 8, 6, 6> transformer;
+    transformer->load_params(&param);
     return 0;
 }
